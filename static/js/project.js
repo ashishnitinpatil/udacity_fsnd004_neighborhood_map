bf84ba87b44cf7449388a6a0b81e132ac629aefb
Ashish Patil <ashishnitinpatil@gmail.com>
2018-04-20 20:23:39 +0400

    ~map resize with event trigger

--- static/js/project.js
+++ static/js/project.js
@@ -1,9 +1,12 @@
 $(document).ready(function() {
     // set height & width of the map div w.r.t. window height & navbar dims
     function setMapDimensions() {
-      mapHeight = $(window).innerHeight() - $('#navbar').innerHeight();
-      $('#map').css('min-height', mapHeight);
-      $('#map').css('max-width', $('#navbar').innerWidth());
+        mapHeight = $(window).innerHeight() - $('#navbar').innerHeight();
+        $('#map').css('min-height', mapHeight);
+        $('#map').css('max-width', $('#navbar').innerWidth());
+        if (typeof google != "undefined") {
+            google.maps.event.trigger(map, "resize");
+        }
     };
     setMapDimensions();
 
@@ -13,6 +16,7 @@ $(document).ready(function() {
 
     $('.navbar-toggler').click(function() {
         $('#sidebar').toggle();
+        setMapDimensions();
     });
 });
 
